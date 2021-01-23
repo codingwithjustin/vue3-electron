bb0e99ea84577d1523b59f1f090d9ff576abf790
Justin Brooks <jsbroks@gmail.com>
2021-01-23 16:36:36 -0500

    chore: update web rules

--- src/background.js
+++ src/background.js
@@ -22,8 +22,7 @@ async function createWindow() {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
-      enableRemoteModule: true,
-      webSecurity: false
+      enableRemoteModule: true
     }
   })
 
